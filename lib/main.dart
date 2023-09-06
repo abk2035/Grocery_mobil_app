1e55461a1145a42cae857f935efe6eb2bb86643a
abk237 <abk@gmail.com>
2023-09-06 06:21:57 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/screens/signIn_screen.dart';
+import 'package:hello_world/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+       colorScheme: ColorScheme.fromSwatch(backgroundColor:const Color(0xFFFEC54B) )
+                               .copyWith(secondary: Colors.deepOrange)
+      ),
+      home: SplashScreen(),
+      routes:{
+        SignInScreen.routeName:(ctx)=>SignInScreen()
+      }
+    );
+  }
+}
+
+
+
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:hello_world/screens/signIn_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+    @override
+  void initState() {
+
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                    SignInScreen()
+            )
+        )
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(decoration: BoxDecoration(color: ),);
+  }
+}
